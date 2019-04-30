5af6df1b3020c9e11faef08c05b9f868dc4c7463
aparikh98 <aparikh64@gmail.com>
2019-04-30 00:41:50 -0700

    some progress

---
+++ part2test.m
@@ -0,0 +1,9 @@
+data = load('part2.mat');
+Q = data.part1.bend_angle;
+u = data.part1.left_pwm;
+gravity = G_gen(Q);
+%G + Kq = a * u
+X = [gravity, Q];
+b = regress(u,X);    % Removes NaN data
+alpha = 1/b(1)
+K = b(2)/b(1)
